511f84b5f53ce7fd6e7c85198cb04e438fd62456
java-SPARK <54886104+java-SPARK@users.noreply.github.com>
2020-05-06 11:55:45 +0800

    策略

--- Client.java
+++ Client.java
@@ -1,36 +1,12 @@
-
-package beiwanglumoshi;
+package celuemoshi;
 
 public class Client {
-	public static void main(String[] args) {
-		OriginatorChess chess=new OriginatorChess(new MementoChess("ºÚ",2,2));
-		play(chess);
-		chess.SetX(3);
-		play(chess);
-		chess.SetX(6);
-		play(chess);
-		undo(chess,index);
-		undo(chess,index);
-		redo(chess,index);
-		redo(chess,index);
+	TravelStrategy travelStrategy;
+	public void setTravelStategy(TravelStrategy travelStrategy) {
+		this.travelStrategy=travelStrategy;
 	}
-	private static int index=0;
-	private static CaretakerChess caretaker=new CaretakerChess();
-	public static void play(OriginatorChess chess) {
-		index++;
-		caretaker.AddMem(chess.SavetoMemento());
-		chess.Show();
-	}
-	public static void undo(OriginatorChess chess,int i) {
-		System.out.println("****»ÚÆå*****");
-		index--;
-		chess.Restore(caretaker.getMem(index-1));
-		chess.Show();
-	}
-	public static void redo(OriginatorChess chess,int i) {
-		System.out.println("***³·Ïú»ÚÆå****");
-		chess.Restore(caretaker.getMem(index));
-		index++;
-		chess.Show();
-	}
+public static void main(String[] args) {
+	HighTrainStragy travel=new HighTrainStragy();
+	travel.travelAlgoritlm();
+}
 }
---
+++ TrainStrategy.java
@@ -0,0 +1,11 @@
+package celuemoshi;
+
+public class TrainStrategy implements TravelStrategy {
+
+	@Override
+	public void travelAlgoritlm() {
+		// TODO 自动生成的方法存根
+		System.out.println("乘坐火车！");
+	}
+
+}
