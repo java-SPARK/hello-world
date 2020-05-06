535a46d199406ed7d4987f49c40259761d88c075
java-SPARK <54886104+java-SPARK@users.noreply.github.com>
2020-05-06 11:55:10 +0800

    备忘录

---
+++ CaretakerChess.java
@@ -0,0 +1,13 @@
+package beiwanglumoshi;
+
+import java.util.ArrayList;
+
+public class CaretakerChess {
+	private ArrayList<MementoChess> memlist=new ArrayList();
+	public void AddMem(MementoChess mem) {
+		memlist.add(mem);
+	}
+	public MementoChess getMem(int i) {
+			return memlist.get(i);
+	}
+}
--- Client.java
+++ Client.java
@@ -1,26 +1,36 @@
-package zuhemoishi;
+
+package beiwanglumoshi;
 
 public class Client {
-
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-		MyElement obj1,obj2,obj3;
-		Plate plate1,plate2,plate3;
-		obj1=new Apple();
-		obj2=new Banana();
-		obj3=new Banana();
-		plate1=new Plate();
-		plate1.add(obj1);
-		plate2=new Plate();
-		plate2.add(obj2);
-		plate2.add(obj3);
-		plate3=new Plate();
-		plate3.add(plate2);
-		plate3.add(plate1);
-		plate3.eat();
-		System.out.println("-------");
-		plate2.delete(obj3);
-		plate3.eat();
+		OriginatorChess chess=new OriginatorChess(new MementoChess("黑",2,2));
+		play(chess);
+		chess.SetX(3);
+		play(chess);
+		chess.SetX(6);
+		play(chess);
+		undo(chess,index);
+		undo(chess,index);
+		redo(chess,index);
+		redo(chess,index);
 	}
-
+	private static int index=0;
+	private static CaretakerChess caretaker=new CaretakerChess();
+	public static void play(OriginatorChess chess) {
+		index++;
+		caretaker.AddMem(chess.SavetoMemento());
+		chess.Show();
+	}
+	public static void undo(OriginatorChess chess,int i) {
+		System.out.println("****悔棋*****");
+		index--;
+		chess.Restore(caretaker.getMem(index-1));
+		chess.Show();
+	}
+	public static void redo(OriginatorChess chess,int i) {
+		System.out.println("***撤销悔棋****");
+		chess.Restore(caretaker.getMem(index));
+		index++;
+		chess.Show();
+	}
 }
